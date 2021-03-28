962a9fcc6f16f714f65753e93d8d026ad11e1c3e
llenax <67596732+llenax@users.noreply.github.com>
2021-03-28 16:41:27 +0300

    edu

---
+++ app.js
@@ -0,0 +1,8 @@
+const fetch = require("node-fetch");
+const urls = ["https://dmp.llenax.repl.co"];
+
+setInterval(() => {
+  urls.forEach((url) => {
+    fetch(url).then((r) => console.log(`${url} | ${r.status}`));
+  });
+}, 60000);
